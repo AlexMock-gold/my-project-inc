0e4ad61302502a19ae00c46745c43df98ce76e4b
AlexMock <mokanua2020@gmail.com>
2024-09-03 22:55:49 +0300

    created section About

---
+++ src/layout/sections/about/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components";
+
+export const About = () => {
+    return (
+        <StyledAbout>
+
+        </StyledAbout>
+    );
+};
+
+const StyledAbout = styled.section`
+    height: 100vh;
+    background-color: #68b068;
+`
+
